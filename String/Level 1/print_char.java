cc6ef620cdea8675f9679f80d9ba9a6adfbdef80
SARTHAK MANE <sarthakmane475@gmail.com>
2026-08-26 22:24:24 +0530

    Add files via upload

---
+++ String/Level 1/print_char.java
@@ -0,0 +1,10 @@
+public class print_char {
+    public static void main(String[] args) {
+
+        String str = "Hello";
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+    }
+}
