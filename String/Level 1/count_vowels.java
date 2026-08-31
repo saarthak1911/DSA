0fdb4967ab6a7049fc64f65b78551796db974e7b
SARTHAK MANE <sarthakmane475@gmail.com>
2026-08-31 22:26:09 +0530

    Add files via upload

---
+++ String/Level 1/count_vowels.java
@@ -0,0 +1,21 @@
+public class count_vowels {
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u') {
+
+                count++;
+            }
+        }
+
+        System.out.println("Vowels = " + count);
+    }
+}
