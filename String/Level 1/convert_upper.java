dd2994fa1a0dcad03bafcf89acb692fbaf4173a4
SARTHAK MANE <sarthakmane475@gmail.com>
2026-09-05 22:53:46 +0530

    Add files via upload

---
+++ String/Level 1/convert_upper.java
@@ -0,0 +1,19 @@
+public class convert_upper {
+    public static void main(String[] args) {
+
+        String str = "hello java";
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                ch = (char)(ch - 32);
+            }
+
+            result += ch;
+        }
+
+        System.out.println(result);
+    }
+}
