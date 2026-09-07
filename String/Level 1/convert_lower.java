7f61ea42ac579c14d45026ad857bc3db8982b483
SARTHAK MANE <sarthakmane475@gmail.com>
2026-09-07 22:22:01 +0530

    Add files via upload

---
+++ String/Level 1/convert_lower.java
@@ -0,0 +1,19 @@
+public class convert_lower {
+    public static void main(String[] args) {
+
+        String str = "HELLO JAVA";
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char)(ch + 32);
+            }
+
+            result += ch;
+        }
+
+        System.out.println(result);
+    }
+}
